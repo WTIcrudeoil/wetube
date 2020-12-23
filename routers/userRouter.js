268c0e21d6bcff5b6f9544e269baa82a65507555
WTIcrudeoil <64414591+WTIcrudeoil@users.noreply.github.com>
2020-12-23 12:57:52 +0900

    CH4-0_video-part_intro-webpack
    video delete,editing
    and install esLint and troubleshooting
    and introduction of webpack

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,10 +1,9 @@
 import express from "express";
-import { changePassword, editProfile, userDetail, users } from "../controllers/userController";
 import routes from "../routes";
+import { changePassword, editProfile, userDetail } from "../controllers/userController";
 
 const userRouter = express.Router();
 
-userRouter.get(routes.users,users);
 userRouter.get(routes.editProfile,editProfile);
 userRouter.get(routes.changePassword,changePassword);
 userRouter.get(routes.userDetail(),userDetail);
